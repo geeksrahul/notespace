60c682173576ff41f0842e34c1e7e4a2dd1ecda0
geeksrahul <geeksrahulofficial@gmail.com>
2025-04-07 14:55:58 +0530

    server : plugging notes routes into express app

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,15 @@
 import express from 'express'
 import dotnev from 'dotenv'
+import cors from 'cors'
+// routers
+import notesRouter from './routes/notes.route.js'
 dotnev.config()
 
 const app = express()
+// middlewares
+app.use(cors())
+app.use(express.json())
+// plugging routers
+app.use("/api/notes", notesRouter)
 
 export default app;
